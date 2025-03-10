d9b6cd79366b7c654b287226713ef3a24521ebe0
Nauczyciel <dqwfe@>
2025-03-10 10:34:57 +0100

    gO

--- classes/Player.py
+++ classes/Player.py
@@ -1,5 +1,6 @@
 from classes.GameObject import GameObject
 from classes.Obstacles import Wall
+import math
 max_health=100
 player_speed=5
 class Player(GameObject):
@@ -9,19 +10,34 @@ class Player(GameObject):
         self.speed=player_speed
         self.vwall = GameObject(self.game, self.game.mouse_pos[0], self.game.mouse_pos[1], 100, 50, None,
                                 color=(100, 0, 0), server=False, visible=False)
+        self.mode = 0
+        self.timer=0
+        self.range=300
     def move(self):
-
+        self.dx=0
+        self.dy=0
         if self.game.w:
             self.y-=self.speed
+            self.dy = -self.speed
         elif self.game.s:
             self.y+=self.speed
+            self.dy = self.speed
         if self.game.a:
             self.x-=self.speed
+            self.dx = -self.speed
         elif self.game.d:
             self.x+=self.speed
-        self.borders()
+            self.dx = self.speed
         self.update()
+        self.borders()
+
     def borders(self):
+        for object in self.game.objects:
+            if type(object)==Wall:
+                if self.rect.colliderect(object.rect):
+                    self.x-=3*self.dx
+                    self.y-=3*self.dy
+
         if self.x<0:
             self.x=0
         if self.x>=self.game.width-self.w:
@@ -31,10 +47,36 @@ class Player(GameObject):
         if self.y>=self.game.height-self.h:
             self.y=self.game.height-self.h
     def plan_wall(self):
+        self.timer+=1
+        if self.game.mouse_pos[0]>=self.x and self.game.mouse_pos[0]<=self.x+self.w:
+            self.mode=0
+        elif self.game.mouse_pos[1]>=self.y and self.game.mouse_pos[1]<=self.y+self.h:
+            self.mode=1
+        if self.mode==1:
+
+            self.vwall.x=self.game.mouse_pos[0]-10
+            self.vwall.y = self.game.mouse_pos[1]-50
+            self.vwall.w = 20
+            self.vwall.h = 100
+            self.vwall.update()
+        elif self.mode==0:
+
+            self.vwall.x = self.game.mouse_pos[0]-50
+            self.vwall.y = self.game.mouse_pos[1]-10
+
+            self.vwall.w = 100
+            self.vwall.h = 20
+            self.vwall.update()
         if self.game.q:
+            print()
 
             self.vwall.visible=True
         else:
             self.vwall.visible = False
+        if math.sqrt((self.x - self.game.mouse_pos[0]) ** 2 + (self.y - self.game.mouse_pos[1]) ** 2) > self.range:
+            self.vwall.visible=False
+
     def build_wall(self):
-        pass
+        if self.timer>=20:
+            self.timer=0
+            Wall(self.game,self.vwall.x,self.vwall.y,self.vwall.w,self.vwall.h,None)
